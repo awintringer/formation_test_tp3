8225fc0e8a14ff513a7232fc87a5ccab6788f257
A Wintringer <awintringer@gmail.com>
2018-09-12 14:17:34 +0200

    du caca

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,8 +1,26 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage
 {
-    public HomePage()
-    {
 
+    WebDriver driver;
+
+    @FindBy(id = "hplogo");
+    WebElement logo;
+
+    @FindBy(name = "q");
+    WebElement barreRecherche;
+
+    @FindBy(css = ".lsb");
+    WebElement boutonRecherche;
+
+    public HomePage(WebDriver driver)
+    {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void recherche(String text)
--- src/test/java/SeleniumDest.java
+++ src/test/java/SeleniumDest.java
@@ -2,14 +2,20 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumDest
 {
 
+    WebDriver driver;
+
     @Before
     public void setup()
     {
-
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
     }
 
     @After
@@ -21,10 +27,10 @@ public class SeleniumDest
     @Test
     public void test_premier()
     {
-        HomePage homePage = new HomePage();
-        homepage.recherche("Bordeaux");
-        ResultPage resultPage = new ResultPage();
+        HomePage homePage = new HomePage(driver);
+        homePage.recherche("Bordeaux");
 
+        ResultPage resultPage = new ResultsPage();
         Assert.assertThat(resultPage.getResult(0), is("Site officiel de la ville de Bordeaux | Bordeaux"));
     }
 }
